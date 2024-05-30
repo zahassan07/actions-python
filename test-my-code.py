897616d04daafc036e48d935f22b1e809d11db0c
Zohaib Ahmed <zahmedhassan@digitalocean.com>
2024-05-30 15:00:28 +0500

    added new files

--- test-my-code.py
+++
@@ -1,8 +0,0 @@
-# test_my_code.py
-from main import add_numbers
-import pytest
-
-def test_add_numbers():
-    assert add_numbers(2, 3) == 5
-    assert add_numbers(-1, 1) == 0
-    assert add_numbers(0, 0) == 0
